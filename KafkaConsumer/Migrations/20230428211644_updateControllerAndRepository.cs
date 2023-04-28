0777a3d25692db4bef2c24634a084e0c5e5a513e
hjal17 <hjal099@gmail.com>
2023-04-28 22:18:13 +0100

    update

---
+++ KafkaConsumer/Migrations/20230428211644_updateControllerAndRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KafkaConsumer.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateControllerAndRepository : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastMaintainedTime",
+                table: "Machines",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "StartDate",
+                table: "Machines",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastMaintainedTime",
+                table: "Machines");
+
+            migrationBuilder.DropColumn(
+                name: "StartDate",
+                table: "Machines");
+        }
+    }
+}
--- KafkaConsumer/Models/Machine.cs
+++ KafkaConsumer/Models/Machine.cs
@@ -4,7 +4,7 @@
     {
         public Guid  Id { get; set; }
         public string Name { get; set; }
-        public string StartDate { get; set; }
+        public DateTime StartDate { get; set; }
         public string Description { get; set; }
         public short MaintainedCount { get; set; }
         public DateTime LastMaintainedTime { get; set; }
